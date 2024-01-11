4995c461fb1f332a94488d3dc19adeb39dcb8a73
kusayvarde <144258449+kusayvarde@users.noreply.github.com>
2024-01-12 01:25:44 +0300

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<title>veritabani</title>
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "okul";
+
+    $conn = mysqli_connect("$servername" ,"$username", "$password", "$dbname");
+    
+    
+    
